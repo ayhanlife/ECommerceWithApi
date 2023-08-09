f231ddf85763482f4a1865094b8b43772863b09e
Ayhan YILDIZ <ayhanyldiz@hotmail.com>
2023-08-09 20:20:49 +0300

    User Dto Added

---
+++ Entities/Dtos/UserDtos/UserDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstracr;
+
+namespace Entities.Dtos.UserDtos
+{
+    public class UserDetailDto :IDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+    }
+}
